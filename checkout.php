6bee618029a8a434440529f43b6f373a52c902e6
Ethan <ethan@ewuddy.com>
2020-01-13 14:27:56 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -22,7 +22,7 @@ $ksdk = new KonnektiveSDK($pageType, $deviceType);
 <html>
 <head>
     <title>
-        Checkout - Rocket Commerce
+        Checkout
     </title>
 
     <meta name="viewport" content="width=device-width"/>
